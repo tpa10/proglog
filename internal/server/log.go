e4c7f0b8cd6d2b59dfe2ea064ec8571e8c194f25
tpa10 <l84lnch@gmail.com>
2021-06-06 18:34:52 -0700

    If you want append to work, it really helps to include the call to append()

--- internal/server/log.go
+++ internal/server/log.go
@@ -19,6 +19,7 @@ func (c *Log) Append(record Record) (uint64, error) {
     c.mu.Lock()
     defer c.mu.Unlock()
     record.Offset = uint64(len(c.records))
+    c.records = append(c.records, record)
     return record.Offset, nil
 }
 
