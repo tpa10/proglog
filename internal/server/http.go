60bfcb17158548d53c5455f6e8b585cd2553da50
tpa10 <l84lnch@gmail.com>
2021-06-06 20:34:25 -0700

    Fix numerous coding errors

--- internal/server/http.go
+++ internal/server/http.go
@@ -52,6 +52,17 @@ func (s *httpServer) handleProduce( w http.ResponseWriter, r *http.Request) {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
+    off, err := s.Log.Append(req.Record)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    res := ProduceResponse{Offset: off}
+    err = json.NewEncoder(w).Encode(res)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
 }
 
 func (s *httpServer) handleConsume(w http.ResponseWriter, r *http.Request) {
