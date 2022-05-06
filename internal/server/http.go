07cf8e0740b245f3143d3008f2c4232e28e1966b
tpa10 <l84lnch@gmail.com>
2022-05-05 20:19:14 -0700

    Clean up internal/server/http.go

--- internal/server/http.go
+++ internal/server/http.go
@@ -45,19 +45,39 @@ type ConsumeResponse struct {
     Record Record `json:"record"`
 }
 
+// Handle request to create a new entry.
 func (s *httpServer) handleProduce( w http.ResponseWriter, r *http.Request) {
     var req ProduceRequest
+
+    /*
+     * Unmarshal the JSON defining the new data to be appended
+     *  into a Record structure.
+     */
     err := json.NewDecoder(r.Body).Decode(&req)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
+
+    /*
+     * Append the new record to the end of the log, retrieving the offset
+     *  (think "key") into the log file where the new record exists.
+     */
     off, err := s.Log.Append(req.Record)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
+
+    /*
+     * Format the offset (aka response data) into a struct
+     */
     res := ProduceResponse{Offset: off}
+
+    /*
+     * Marshal the response data into JSON format directly into the 
+     *  http response writer (w)
+     */
     err = json.NewEncoder(w).Encode(res)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -65,23 +85,49 @@ func (s *httpServer) handleProduce( w http.ResponseWriter, r *http.Request) {
     }
 }
 
+// Handle request to fetch a record.
 func (s *httpServer) handleConsume(w http.ResponseWriter, r *http.Request) {
     var req ConsumeRequest
+
+    /*
+     * Un-Marshall the JSON formatted request data (offset/key) into a struct
+     */
     err := json.NewDecoder(r.Body).Decode(&req)
     if err != nil {
         http.Error(w, err.Error(), http.StatusBadRequest)
         return
     }
+
+    /*
+     * Use the provided offset to try and read a record.
+     */
     record, err := s.Log.Read(req.Offset)
+    
+    /*
+     * If the offset doesn't exist, we have a "Page not found" condition
+     *  User, not operational or software error, so we give them the old
+     *  "404" finger and move on.
+     */
     if err == ErrOffsetNotFound {
         http.Error(w, err.Error(), http.StatusNotFound)
         return
     }
+
+    /*
+     * If we get a different error (e.g. read, permissions, etc) 
+     *  then we have some kind of opertional problem or a bug,
+     *  so we blow them a "500" snotgram and move on. 
+     *  (Prepare for the support call)
+     */
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
+
+    // Format the returned data into a struct
     res := ConsumeResponse{Record: record}
+
+    // Marshal the struct into JSON and send it to the http response writer
     err = json.NewEncoder(w).Encode(res)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
